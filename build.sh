946e5f24e15adec8794138095195e74e43de1642
Chan4077 <edric.chan.1997@gmail.com>
2018-08-19 21:21:00 +0800

    chore: remove initial width and height for all whiptail dialogs

--- build.sh
+++ build.sh
@@ -18,14 +18,14 @@ ftpUploadOptions=()
 
 # Function for setting the configuration of FTP
 ftpConfigDialog() {
-  whiptail --yesno "Would you like to upload the builds via FTP?" 10 25
+  whiptail --yesno "Would you like to upload the builds via FTP?" 0 0
   if [[ $? -eq 0 ]]; then
     if [[ "$FTP_UPLOAD_OPTIONS" ]]; then
       ftpUploadOptions=($FTP_UPLOAD_OPTIONS)
     elif [[ "$FTP_UPLOAD_OPTS" ]]; then
       ftpUploadOptions=($FTP_UPLOAD_OPTS)
     else
-      ftpUploadOptions=($(whiptail --checklist "Choose the types of files to upload:" 20 80 7 \
+      ftpUploadOptions=($(whiptail --checklist "Choose the types of files to upload:" 0 0 0 \
       "ROM" "The resultant of the building process" ON \
       "ROM_OTA" "The OTA (over-the-air) build" ON \
       "ROM_IMAGE" "Images (all files that have an img extension)" OFF \
@@ -34,17 +34,17 @@ ftpConfigDialog() {
     if [[ "$FTP_SERVER" ]]; then
       ftpServer="$FTP_SERVER"
     else
-      ftpServer=$(whiptail --inputbox "Enter the FTP server:" 10 40 "uploads.androidfilehost.com" 3>&1 1>&2 2>&3)
+      ftpServer=$(whiptail --inputbox "Enter the FTP server:" 0 0 "uploads.androidfilehost.com" 3>&1 1>&2 2>&3)
     fi
     if [[ "$FTP_USERNAME" ]]; then
       ftpUsername="$FTP_USERNAME"
     else
-      ftpUsername=$(whiptail --inputbox "Enter your username for the FTP server:" 10 40 3>&1 1>&2 2>&3)
+      ftpUsername=$(whiptail --inputbox "Enter your username for the FTP server:" 0 0 3>&1 1>&2 2>&3)
     fi
     if [[ "$FTP_PASSWORD" ]]; then
       ftpPassword="$FTP_PASSWORD"
     else
-      ftpPassword=$(whiptail --passwordbox "Enter your password for the FTP server:" 10 40 3>&1 1>&2 2>&3)
+      ftpPassword=$(whiptail --passwordbox "Enter your password for the FTP server:" 0 0 3>&1 1>&2 2>&3)
     fi
   else
     doneExec
@@ -53,7 +53,7 @@ ftpConfigDialog() {
 
 # Function for showing an input dialog on what device codenames to build
 buildDialog() {
-  devices=$(whiptail --inputbox "Enter a list of device codenames. Separate each device codename by a space." 10 40 3>&1 1>&2 2>&3)
+  devices=$(whiptail --inputbox "Enter a list of device codenames. Separate each device codename by a space." 0 0 3>&1 1>&2 2>&3)
   build $devices
   ftpConfigDialog
   for i in "${#outdirs[@]}";
@@ -64,15 +64,15 @@ buildDialog() {
       do
         if [[ optionsI == "ROM" ]]; then
           rom=$(ls -tr ${outdirs[i]/lineage-*.zip} | tail -1)
-          ftpLocation=$(whiptail --inputbox "Enter the folder path of where the build for device ${devices[i]} will be uploaded to." 10 40 3>&1 1>&2 2>&3)
+          ftpLocation=$(whiptail --inputbox "Enter the folder path of where the build for device ${devices[i]} will be uploaded to." 0 0 3>&1 1>&2 2>&3)
           ftpUpload $ftpServer $rom $ftpLocation $ftpUsername $ftpPassword
         elif [[ optionsI == "ROM_OTA" ]]; then
           romOTA=$(ls -tr ${outdirs[i]/lineage_*-ota-*.zip} | tail -1)
-          ftpLocation=$(whiptail --inputbox "Enter the folder path of where the OTAs for device ${devices[i]} will be uploaded to." 10 40 3>&1 1>&2 2>&3)
+          ftpLocation=$(whiptail --inputbox "Enter the folder path of where the OTAs for device ${devices[i]} will be uploaded to." 0 0 3>&1 1>&2 2>&3)
           ftpUpload $ftpServer romOTA $ftpLocation $ftpUsername $ftpPassword
         elif [[ optionsI == "ROM_IMAGE" ]]; then
           romImage=($(ls ${outdirs[i]/*.img}))
-          ftpLocation=$(whiptail --inputbox "Enter the folder path of where the images for device ${devices[i]} will be uploaded to." 10 40 3>&1 1>&2 2>&3)
+          ftpLocation=$(whiptail --inputbox "Enter the folder path of where the images for device ${devices[i]} will be uploaded to." 0 0 3>&1 1>&2 2>&3)
           for imageI in "${romImage[@]}";
           do
             ftpUpload $ftpServer $imageI $ftpLocation $ftpUsername $ftpPassword
@@ -85,7 +85,7 @@ buildDialog() {
 
 # Function for showing a confirmation dialog when a command has finished executing
 doneExec() {
-  whiptail --yesno "Go back to main menu?" 10 25
+  whiptail --yesno "Go back to main menu?" 0 0
   if [[ $? -eq 0 ]]; then
     mainMenu
   else
@@ -96,7 +96,7 @@ doneExec() {
 
 # Function for showing a confirmation dialog on whether to exit the program
 confirmExit() {
-  whiptail --yesno "Are you sure you want to exit?" 10 25
+  whiptail --yesno "Are you sure you want to exit?" 0 0
   if [[ $? -eq 0 ]]; then
     infoBold "Exiting..."
     exit 0
@@ -124,7 +124,7 @@ mainMenuHandler() {
     buildDialog
     doneExec
   elif [[ "$menuResult" = "Help" ]]; then
-    whiptail --title "Notice" --msgbox "The help guide is coming soon! Stay tuned." 10 25
+    whiptail --title "Notice" --msgbox "The help guide is coming soon! Stay tuned." 0 0
     mainMenu
   fi
 }
@@ -132,7 +132,7 @@ mainMenuHandler() {
 # Function for showing a menu when the program has been executed
 mainMenu() {
   choices=("Exit" "Quit the script." "Sync" "Sync the Android Source." "Build" "Build for a device(s)." "Help" "Show help.")
-  results=$(whiptail --title "Utilities" --menu "Choose one of the options below:" 15 50 0  "${choices[@]}" 3>&1 1>&2 2>&3)
+  results=$(whiptail --title "Utilities" --menu "Choose one of the options below:" 0 0 0  "${choices[@]}" 3>&1 1>&2 2>&3)
   mainMenuHandler "$results"
 }
 
