cdd304e6240131651467faf88b7b063c79d62a2a
Chan4077 <edric.chan.1997@gmail.com>
2018-09-09 22:06:13 +0800

    functions.sh: Add support for clearing previous builds and target files

--- functions.sh
+++ functions.sh
@@ -129,6 +129,38 @@ checkFunction() {
   fi
 }
 
+# Function for clearing all lineage-15.1-* files in the out directory (includes md5sum files)
+clearPrevBuilds() {
+  outDir="$1"
+  if [[ -n "$CLEAR_PREV_BUILDS" ]]; then
+    if [[ "$CLEAR_PREV_BUILDS" = true ]]; then
+      infoBold "Clearing previous LineageOS builds..."
+      # See https://stackoverflow.com/a/26765276
+      ls -t "$outDir/lineage*" | tail -n +4 | xargs rm --
+    fi
+  else
+    infoBold "Clearing previous LineageOS builds..."
+    # See https://stackoverflow.com/a/26765276
+    ls -t "$outDir/lineage*" | tail -n +4 | xargs rm --
+  fi
+}
+
+# Clears the previous target file which can take up 1+ GB for every build
+clearPrevTargetFiles() {
+  outDir="$1"
+  if [[ -n "$CLEAR_PREV_TARGET_FILES" ]]; then
+    if [[ "$CLEAR_PREV_TARGET_FILES" = true ]]; then
+      infoBold "Clearing previous target files..."
+      # See https://stackoverflow.com/a/26765276
+      ls -t "$outDir/obj/PACKAGING/target_files_intermediates" | tail -n +4 | xargs rm -r --
+    fi
+  else
+    infoBold "Clearing previous target files..."
+    # See https://stackoverflow.com/a/26765276
+    ls -t "$outDir/obj/PACKAGING/target_files_intermediates" | tail -n +4 | xargs rm -r --
+  fi
+}
+
 # Function to sync
 sync() {
   infoBold "Syncing the latest changes..."
@@ -177,8 +209,9 @@ buildDevices() {
           infoBold "Building for $i..."
           breakfast "$i"
           brunch "$i"
+          clearPrevBuilds "$OUT"
+          clearPrevTargetFiles "$OUT"
           outdirs+=($OUT)
-          echo "${outdirs[@]}"
       done
     else
       errorBold "The functions breakfast and/or brunch do not exist. Have you sourced the AOSP and LineageOS build tools?"
